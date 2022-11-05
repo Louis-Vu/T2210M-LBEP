8e1bf4d2aca42b99628db6074cb75f831a899177
Louis-Vu <hoangvuhuy04@gmail.com>
2022-11-05 15:27:16 +0700

    Update bai3.cpp

--- bai tap #/bai3.cpp
+++ bai tap #/bai3.cpp
@@ -1,25 +1,30 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 int main(){
-	char a[50];
-	char b[50];
-	printf("nhap chuoi a:");
+	char a[50],b[20];
+	printf("Nhap chuoi a:");
 	scanf("%s",a);
-	printf("nhap chuoi b:");
+	printf("Nhap chuoi b:");
 	scanf("%s",b);
-	int f=0;
-    for(int i=0;i<= b[100];i++){
-    	if(strchr(a,b[i])){
-    		f=1;
-        }else{
-        	f=0;
-        	break;
+	int c = 0;
+
+	for(int i=0;i<strlen(a);i++){
+		if(a[i] == b[0]){
+			for(int j=0;j<strlen(b);j++){
+				if(a[i+j]== b[j]){
+					c++;
+				}
+			}
+		}
+		if(c==strlen(b)){
+			break;
+		}else{
+			c=0;
 		}
 	}
-	if(f==1){
-		printf("chuoi b co xuat hien trong chuoi a");
-	}
-	else{
-	printf("chuoi b khong xuat hien trong chuoi a");
+	if(c==strlen(b)){
+		printf("%s nam trong %s",b,a);
+	}else{
+		printf("%s ko nam trong %s",b,a);
 	}
 }
